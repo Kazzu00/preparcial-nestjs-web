f4f6a8c6cb3451e399f7cfd4bec91a9c26793167
Kazzu00 <j.solorzanod@uniandes.edu.co>
2025-11-21 10:14:56 -0500

    Parcial Parte B

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountriesModule } from './countries/countries.module';
 import { TravelPlansModule } from './travel-plans/travel-plans.module';
 import { Country } from './countries/entities/country.entity';
 import { TravelPlan } from './travel-plans/entities/travel-plan.entity';
+import { LoggerMiddleware } from './common/middleware/logger.middleware'; // Importamos el middleware
 
 @Module({
   imports: [
-    // Configuración de la base de datos SQLite 
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'travel_db.sqlite', 
+      database: 'travel_db.sqlite',
       entities: [Country, TravelPlan],
-      synchronize: true, 
+      synchronize: true,
     }),
     CountriesModule,
     TravelPlansModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // Middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('countries', 'travel-plans'); 
+  }
+}
